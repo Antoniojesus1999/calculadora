a74365f0329330644ec10d4a821db76ed355d675
Antonio Jesús <antoniojesuspv99@gmail.com>
2023-05-15 01:24:04 +0200

    Implementación logger

---
+++ src/main/java/com/iteriam/sanitas/calculadora/log/impl/LoggerImpl.java
@@ -0,0 +1,27 @@
+package com.iteriam.sanitas.calculadora.log.impl;
+
+import com.iteriam.sanitas.calculadora.log.Logger;
+import io.corp.calculator.TracerImpl;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoggerImpl implements Logger {
+
+    private TracerImpl tracerImpl;
+
+    /**
+     * Empty constructor
+     */
+    public LoggerImpl() {
+        this.tracerImpl = new TracerImpl();
+    }
+
+    private TracerImpl getTracerImpl() {
+        return tracerImpl == null ?  new TracerImpl() : tracerImpl;
+    }
+
+    @Override
+    public void log(String message) {
+        getTracerImpl().trace(message);
+    }
+}
